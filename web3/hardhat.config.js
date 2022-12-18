f4a5d875f3f816cb9746ab7599676b76d4fb4e0e
nithin790 <113250052+nithin790@users.noreply.github.com>
2022-12-18 00:44:12 -0600

    FIRST COMMIT
    A 3.0 web base application with us of solidity

---
+++ web3/hardhat.config.js
@@ -0,0 +1,20 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork: 'goreli',
+    networks:{
+      hardhat:{},
+      goreli:{
+        url:'https://rpc.ankr.com/eth_goreli',
+        accounts:['0x${process.env.PRIVATE_KEY}']
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
